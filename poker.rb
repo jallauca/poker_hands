1232e41bdab6d20eb1c8acfefc3dd0a84a211338
Jaime Allauca ext:(%3E) <jallauca@ipcoop.com ?mailto:jallauca@ipcoop.com>
2014-07-03 05:51:22 -0400

    Add input tests

--- poker.rb
+++ poker.rb
@@ -1,5 +1,15 @@
 #!/usr/bin/env ruby
 
+def input_to_hands(input)
+  split_hands = input.scan(/\w+:(?:\s[0-9AJKQ][HDSC]){5}/)
+  split_hash = split_hands.reduce({ }) do |h, s|
+    name, cards = s.split(":")
+    h[ name ] = cards.strip.split(" ")
+    h
+  end
+  split_hash
+end
+
 def script_main
   tests_flag = false
   ARGV.each do |value|
@@ -16,17 +26,28 @@ def show_tests
         'White' => [ '2C', '3H', '4S', '8C', 'AH' ]
       }
   end
-  puts 'tests pass'
-end
-
-def input_to_hands(input)
-  split_hands = input.scan(/\w+:(?:\s[0-9AJKQ][HDSC]){5}/)
-  split_hash = split_hands.reduce({ }) do |h, s|
-    name, cards = s.split(":")
-    h[ name ] = cards.strip.split(" ")
-    h
+  assert do 
+    input_to_hands("Black: 2H 4S 4C 2D 4H White: 2S 8S AS QS 3S") ==
+      {
+        'Black' => [ '2H', '4S', '4C', '2D', '4H' ],
+        'White' => [ '2S', '8S', 'AS', 'QS', '3S' ]
+      }
   end
-  split_hash
+  assert do 
+    input_to_hands("Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C KH") ==
+      {
+        'Black' => [ '2H', '3D', '5S', '9C', 'KD' ],
+        'White' => [ '2C', '3H', '4S', '8C', 'KH' ]
+      }
+  end
+  assert do 
+    input_to_hands("Black: 2H 3D 5S 9C KD White: 2D 3H 5C 9S KH") ==
+      {
+        'Black' => [ '2H', '3D', '5S', '9C', 'KD' ],
+        'White' => [ '2D', '3H', '5C', '9S', 'KH' ]
+      }
+  end
+  puts 'tests pass'
 end
 
 class AssertionError < RuntimeError
