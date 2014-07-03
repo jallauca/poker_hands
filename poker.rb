db7b4c072c17b6a917f4dcee3b061ed79728fad1
Jaime Allauca ext:(%3E) <jallauca@ipcoop.com ?mailto:jallauca@ipcoop.com>
2014-07-03 06:15:29 -0400

    Determine winner in high card

--- poker.rb
+++ poker.rb
@@ -9,15 +9,61 @@ def input_to_hands(input)
   end
 end
 
+def winner(hands)
+  hand_scores = hands.reduce({ }) do |memo, (name, cards)|
+    score = get_cards(cards)
+    memo[name] = score
+    memo
+  end
+
+  won = hand_scores.max { |hand1, hand2| hand_comparison(hand1, hand2) }
+  won[0]
+end
+
+@indexed_cards = "0 1 2 3 4 5 6 7 8 9 10 J Q K A".split(" ")
+
+def get_cards(cards)
+  cards.map { |c| @indexed_cards.index(c[0]) }.sort.reverse
+end
+
+def hand_comparison(hand1, hand2)
+  _, score1 = hand1
+  _, score2 = hand2
+  
+  cmp = 0
+  score1.each_with_index do |c, i|
+    cmp = score1[i] <=> score2[i]
+    break unless cmp == 0
+  end
+
+  cmp
+end
+
 def script_main
   tests_flag = false
+  input = ""
+
   ARGV.each do |value|
     tests_flag = true if value == '--tests'
   end
-  show_tests if tests_flag
+
+  return show_tests if tests_flag
+  return show_help unless ARGV.count == 1
+
+  input_to_hands(ARGV[0])
+end
+
+def show_help
+  puts "poker \"input\""
+  puts "poker --tests\t\trun tests"
 end
 
 def show_tests
+  input_to_hands_tests
+  winner_tests
+end
+
+def input_to_hands_tests
   assert do 
     input_to_hands("Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH") ==
       {
@@ -47,6 +93,14 @@ def show_tests
       }
   end
   puts 'tests pass'
+end
+
+def winner_tests
+  assert do 
+    w = winner(input_to_hands("Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH"))
+    puts "w wins"
+    w == "White"
+  end
 end
 
 class AssertionError < RuntimeError
