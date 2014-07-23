df690c466cf866c8ea9a92251eed1cc231a19ce7
Jaime Allauca <jallauca@yahoo.com>
2014-07-23 10:48:26 -0400

    Add tests for methods_compose... functionality

--- spec/underscore_spec.rb
+++ spec/underscore_spec.rb
@@ -58,4 +58,66 @@ describe Underscore do
       expect { composition[ "Alicia" ] }.to raise_error
     end
   end
+
+  context "methods" do
+    before do
+      class TestObject
+        def self.name() "Alicia" end
+        def self.greet(name) "Hello, #{name}!" end
+        def self.goodbye(intro) "#{intro} Good bye!" end
+        def self.name_with_args(x) x end
+        def self.name_with_two_args(n, ln) "#{n} #{ln}" end
+      end
+    end
+
+    it "compose one function" do
+      composition = Underscore.methods_compose(TestObject, :name)
+      expect(composition[]).to eq("Alicia")
+    end
+
+    it "compose two functions" do
+      composition = Underscore.methods_compose(TestObject, :greet, :name)
+      expect(composition[]).to eq("Hello, Alicia!")
+    end
+
+    it "compose three functions" do
+      composition = Underscore.methods_compose(TestObject, :goodbye, :greet, :name)
+      expect(composition[]).to eq("Hello, Alicia! Good bye!")
+    end
+
+    it "compose right one function" do
+      composition = Underscore.methods_compose_right(TestObject, :name)
+      expect(composition[]).to eq("Alicia")
+    end
+
+    it "compose right two functions" do
+      composition = Underscore.methods_compose_right(TestObject, :name, :greet)
+      expect(composition[]).to eq("Hello, Alicia!")
+    end
+
+    it "compose right three functions" do
+      composition = Underscore.methods_compose_right(TestObject, :name, :greet, :goodbye)
+      expect(composition[]).to eq("Hello, Alicia! Good bye!")
+    end
+
+    it "compose right three functions and one arg" do
+      composition = Underscore.methods_compose_right(TestObject, :name_with_args, :greet, :goodbye)
+      expect( composition[ "Francesca" ] ).to eq("Hello, Francesca! Good bye!")
+    end
+
+    it "compose right three functions and two args" do
+      composition = Underscore.methods_compose_right(TestObject, :name_with_two_args, :greet, :goodbye)
+      expect( composition[ "Alicia", "Keys" ] ).to eq("Hello, Alicia Keys! Good bye!")
+    end
+
+    it "compose right three functions and one arg skipped" do
+      composition = Underscore.methods_compose_right(TestObject, :name_with_args, :greet, :goodbye)
+      expect { composition[ ] }.to raise_error
+    end
+
+    it "compose right three functions and one of two args skipped" do
+      composition = Underscore.methods_compose_right(TestObject, :name_with_two_args, :greet, :goodbye)
+      expect { composition[ "Alicia" ] }.to raise_error
+    end
+  end
 end
